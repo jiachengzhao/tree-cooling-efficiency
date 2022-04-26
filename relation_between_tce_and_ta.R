266ffe515b52b2c9a4daab7183a0d8c68be12ebb
zhaojiacheng <zhaojiacheng@mail.bnu.edu.cn>
2022-04-26 21:33:48 +0800

    relation between tce and ta

---
+++ relation_between_tce_and_ta.R
@@ -0,0 +1,55 @@
+## ----------------------------------
+# Title: Relationship between tree cooling efficiency (TCE) and air temperature (Ta)
+# Objective: Visualization the linear relationship between TCE and Ta
+# Created by: Jiacheng Zhao
+# Created on: 2022-02-21
+# Copyright (c) Jiacheng Zhao, 2021
+# Beijing Normal University
+# Email: zhaojiacheng@mail.bnu.edu.cn
+## ----------------------------------
+
+# data (2010) ----
+relation = tce.filtered[[3]]
+# city id and name
+name = data.table(
+  id = c(10533, 41448, 15418, 11166, 26262, 9505, 21855, 30077, 37520, 32068, 12490, 26704, 1578, 34896, 23620, 12398),
+  name = c(
+    'Brussels', 'Dallas', 'Paris', 'Rio de Janeiro', 'Lisbon', 'Perth', 'Rome',
+    'Valencia', 'Detroit', 'Kyiv', 'Montreal', 'Moscow', 'Beijing', 'Denver', 'Seoul', 'Quebec City'
+  )
+)
+
+
+# par ----
+mypar(
+  mai = c(0.1, 0.15, 0.1, 0.1),
+  mfcol = c(4, 4),
+  oma = c(3, 12, 2, 2),
+  tck = 0.025
+)
+
+
+# plotting ----
+for (i in 1:nrow(name)) {
+  d = relation[id == name$id[i], .(air_temperature, tce.10)]
+  d[, tce.10 := abs(tce.10)]
+  jplot(
+    data = d, axis.style = 'r',
+    ylim = c(0, 0.6),
+    at2 = seq(0, 0.6, 0.1),
+    ann = F
+  )
+  points(d$air_temperature, abs(d$tce.10), pch = 21, bg = 'plum1', cex = 1.2)
+  fit = MASS::rlm(abs(d$tce.10) ~ air_temperature, data = d)
+  abline(fit, lwd = 0.1)
+  ci95(fit, data.frame(air_temperature = seq(min(d$air_temperature), max(d$air_temperature), 0.1)))
+  if (i %in% seq(4, 16, 4)) mtext(1, text = expression('T'[a] * ' (' * degree * 'C)'), line = 1.3, cex = 0.7, las = 0)
+  if (i %in% 1:4) mtext(2, text = expression('TCE.10 (' * degree * 'C/%)'), line = 1.8, cex = 0.7, las = 0)
+  title(name[, name][i], adj = 0.955, line = -8, font.main = 1, col.main = 'black', cex.main = 1.1)
+  title(addeq(fit, 3), adj = 0.18, line = -0.8, col.main = 'black', cex.main = 1.1)
+  title(addr2(d$tce.10, predict(fit)), adj = 0.085, line = -1.8, col.main = 'black', cex.main = 1.1)
+}
+
+
+# opar ----
+par(opar)
