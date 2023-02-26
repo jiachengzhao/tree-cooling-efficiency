6905edd709bdcab1fa27a62eda9db829a8b1c591
jiachengzhao <zhaojiacheng@mail.bnu.edu.cn>
2023-02-26 23:12:58 +0800

    format code

--- contribution.R
+++ contribution.R
@@ -9,6 +9,7 @@
 ## ----------------------------------
 
 
+
 # BRT data ----
 data.brt = data.table(
   id = re$id, # city id
@@ -37,14 +38,14 @@ data.brt = data.table(
   mswx_solar = apply(re[, grep('mswx_solar_radiation', names(re), value = F), with = F], 1, mean, na.rm = T), # solar radiation [W/m^2]
   mswx_vpd = apply(re[, grep('mswx_vapor_pressure_deficit', names(re), value = F), with = F], 1, mean, na.rm = T), # VPD [kPa]
   mswx_ws = apply(re[, grep('mswx_wind_speed', names(re), value = F), with = F], 1, mean, na.rm = T), # wind speed [m/s]
-  trend_lai = apply(re[, grep('lai_slope', names(re), value = F), with = F], 1, mean, na.rm = T) # wind speed [m/s]
+  trend_lai = apply(re[, grep('lai_slope', names(re), value = F), with = F], 1, mean, na.rm = T) # trend in LAI [-/yr]
 )
 data.brt = data.brt[complete.cases(data.brt)]
 data.brt = data.brt[cloud < 0.7] # cloud cover should be less than 70%
 data.brt = merge(data.brt, esd, by = 'id')[sd < 100] # elevation sd should be less than 100 (m)
 hist(data.brt$tce.10.25)
-# data.brt = data.brt[tce.10.25 < 0.5] # remove outliers
 fwrite(data.brt, './data_brt.csv')
+
 
 
 # BRT using ERA5 and MODIS LAI ----
@@ -70,7 +71,8 @@ dismo::gbm.plot(
 )
 gbms.era5$var.names
 bias(data.brt$tce.10.25, gbms.era5$fit)
-finalVariableOrder = c('lai', 'albedo', 'vpd', 'solar', 'cloud', 'trend_lai', 'ws')
+finalVariableOrder = c('lai', 'albedo', 'vpd', 'solar', 'cloud', 'ws', 'trend_lai')
+
 
 
 # BRT using ERA5 and Landsat LAI ----
@@ -95,8 +97,9 @@ dismo::gbm.plot(
   plot.layout = c(2, 4)
 )
 gbms.era5.landsat$var.names = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
-gbms.era5.landsat$contributions$var = c('lai', 'albedo', 'solar', 'vpd', 'trend_lai', 'cloud', 'ws')
+gbms.era5.landsat$contributions$var = c('lai', 'albedo', 'solar', 'vpd', 'cloud', 'ws', 'trend_lai')
 bias(data.brt$tce.10.25, gbms.era5.landsat$fit)
+
 
 
 # BRT using MSWX and MODIS LAI ----
@@ -121,8 +124,10 @@ dismo::gbm.plot(
   plot.layout = c(2, 4)
 )
 gbms.mswx$var.names = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
-gbms.mswx$contributions$var = c('lai', 'albedo', 'solar', 'trend_lai', 'vpd', 'cloud', 'ws')
+gbms.mswx$contributions$var = c('lai', 'albedo', 'solar', 'vpd', 'cloud', 'trend_lai', 'ws')
 bias(data.brt$tce.10.25, gbms.mswx$fit)
+
+
 
 # BRT using MSWX and Landsat LAI ----
 set.seed(1)
@@ -146,8 +151,9 @@ dismo::gbm.plot(
   plot.layout = c(2, 4)
 )
 gbms.mswx.landsat$var.names = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
-gbms.mswx.landsat$contributions$var = c('lai', 'albedo', 'solar', 'vpd', 'trend_lai', 'cloud', 'ws')
+gbms.mswx.landsat$contributions$var = c('lai', 'albedo', 'solar', 'vpd', 'cloud', 'ws', 'trend_lai')
 bias(data.brt$tce.10.25, gbms.mswx.landsat$fit)
+
 
 
 # Plotting partial effects ----
@@ -162,6 +168,7 @@ marginalData = function(gbms) {
   }
   return(ml)
 }
+
 
 ## normalized marginal data ----
 # gbms ------------- target gbms to be normalized
@@ -184,6 +191,7 @@ marginalData.normalization = function(gbms, gbms.margin) {
   return(ml)
 }
 
+
 ## marginal data using ERA5 and MODIS LAI ----
 # data.margin.era5 = marginalData.normalization(gbms.era5, data.margin.era5.landsat)
 data.margin.era5 = marginalData(gbms.era5)
@@ -192,6 +200,7 @@ data.margin.era5 = marginalData.normalization(gbms.era5, data.margin.era5)
 names(data.margin.era5) = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
 data.margin.era5 = data.margin.era5[finalVariableOrder]
 
+
 ## marginal data using ERA5 and Landsat LAI ----
 data.margin.era5.landsat = marginalData(gbms.era5.landsat)
 names(data.margin.era5.landsat) = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
@@ -199,12 +208,14 @@ data.margin.era5.landsat = marginalData.normalization(gbms.era5.landsat, data.ma
 names(data.margin.era5.landsat) = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
 data.margin.era5.landsat = data.margin.era5.landsat[finalVariableOrder]
 
+
 ## marginal data using MSWX and MODIS LAI ----
 data.margin.mswx = marginalData(gbms.mswx)
 names(data.margin.mswx) = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
 data.margin.mswx = marginalData.normalization(gbms.mswx, data.margin.mswx)
 names(data.margin.mswx) = c('lai', 'trend_lai', 'vpd', 'solar', 'ws', 'albedo', 'cloud')
 data.margin.mswx = data.margin.mswx[finalVariableOrder]
+
 
 ## settings ----
 ### par ----
@@ -231,8 +242,8 @@ xlims = list(
   c(0, 2.1),
   c(0, 320),
   c(0.1, 0.75),
-  c(-0.02, 0.06),
-  c(-0.25, 5.4)
+  c(-0.25, 5.4),
+  c(-0.02, 0.06)
 )
 
 ### ylim ----
@@ -242,8 +253,8 @@ ylims.margin = list(
   c(-0.03, 0.07),
   c(-0.04, 0.08),
   c(-0.02, 0.02),
-  c(-0.03, 0.03),
-  c(-0.02, 0.04)
+  c(-0.02, 0.04),
+  c(-0.03, 0.03)
 )
 
 ### ylim (density plot) ----
@@ -253,8 +264,8 @@ ylims.density = list(
   c(0, 1.7),
   c(0, 0.015),
   c(0, 5.3),
-  c(0, 52),
-  c(0, 0.94)
+  c(0, 0.94),
+  c(0, 52)
 )
 
 ### at1 ----
@@ -264,8 +275,8 @@ at1 = list(
   c(-10, seq(0.3, 2.5, 0.5), 10),
   c(-10, seq(50, 350, 70), 1000),
   c(-1, seq(0.2, 1, 0.15), 2),
-  c(-10, seq(-0.02, 0.04, 0.02), 10),
-  c(-10, seq(0.8, 10, 1.2), 100) 
+  c(-10, seq(0.8, 10, 1.2), 100),
+  c(-10, seq(-0.02, 0.06, 0.02), 10)
 )
 
 ### at2 ----
@@ -275,8 +286,8 @@ at2 = list(
   c(-1, seq(-0.03, 0.07, 0.02), 1),
   c(-1, seq(-0.04, 0.1, 0.03), 1),
   c(-1, seq(-0.03, 0.1, 0.01), 1),
-  c(-1, seq(-0.04, 0.1, 0.02), 1),
-  c(-1, seq(-0.02, 0.04, 0.02), 1)
+  c(-1, seq(-0.02, 0.04, 0.02), 1),
+  c(-1, seq(-0.04, 0.1, 0.02), 1)
 )
 
 ### x-label ----
@@ -286,8 +297,8 @@ var.names = c(
   'VPD (kPa)',
   expression('Solar radiation (W/m'^2 * ')'),
   'Cloud cover',
-  'Trend in LAI (-/yr)',
-  'Wind speed (m/s)'
+  'Wind speed (m/s)',
+  'Trend in LAI (-/yr)'
 )
 
 
@@ -351,13 +362,13 @@ for (i in 1:length(data.margin.era5)) {
   }
   
   # line
-  if (i %in% c(3, 4, 5, 7)) {
+  if (i %in% c(3, 4, 5, 6)) {
     
     if (i == 4) {
       lines(smooth.spline(data.margin.mswx[[i]], spar = 1), col = cols[i], lwd = 1.5, lty = 'dashed')
     } else if (i == 5) {
       lines(smooth.spline(data.margin.mswx[[i]], spar = 1), col = cols[i], lwd = 1.5, lty = 'dashed')
-    } else if (i == 7) {
+    } else if (i == 6) {
       lines(smooth.spline(data.margin.mswx[[i]], spar = 0.9), col = cols[i], lwd = 1.5, lty = 'dashed')
     } else {
       lines(smooth.spline(data.margin.mswx[[i]], spar = 0.8), col = cols[i], lwd = 1.5, lty = 'dashed')
@@ -396,10 +407,10 @@ for (i in 1:length(data.margin.era5)) {
   }
   
   # x-label
-  mtext(1, text = var.names[i], line = label.line + 0.5, cex = cex.font - 0.3)
+  mtext(1, text = var.names[i], line = label.line + 0.5, cex = cex.font - 0.35)
   
   # y-label
-  if (i %in% c(1, 5)) mtext(2, text = expression('Partial effect on TCE.10.25 (' * degree * 'C/%)'), line = label.line + 1.8, adj = 0.7, cex = cex.font - 0.3, las = 0)
+  if (i %in% c(1, 5)) mtext(2, text = expression('Partial effect on TCE.10.25 (' * degree * 'C/%)'), line = label.line + 1.8, cex = cex.font - 0.35, las = 0)
   
   # figure number
   mtext(3, text = substitute(bold(letter), list(
@@ -407,6 +418,7 @@ for (i in 1:length(data.margin.era5)) {
   )), line = -1.5, adj = 0.035, cex = cex.font - 0.3)
   
 }
+
 
 
 # Plotting variable contribution ----
@@ -420,10 +432,12 @@ setDT(contribution)
 colnames(contribution) = c('var', 'era5.m', 'era5.l', 'era5.m.m', 'era5.m.l')
 contribution[, c('mean', 'sd') := .(rowMeans(.SD), apply(.SD, 1, sd)), .SDcols = c('era5.m', 'era5.l', 'era5.m.m', 'era5.m.l')][order(mean, decreasing = T)]
 
+
 ## contribution given by ERA5 + MODIS LAI ----
 contribution.era5 = gbms.era5$contributions
 setDT(contribution.era5)
 contribution.era5$var = factor(contribution.era5$var, levels = contribution.era5$var)
+
 
 ## barplot ----
 bp = barplot(
@@ -431,9 +445,11 @@ bp = barplot(
   data = contribution.era5,
   axes = F, ann = F,
   xaxt = 'n', yaxs = 'i',
+  xlim = c(-0.2, 8.8),
   ylim = c(0, 30),
   col = cols
 )
+
 
 ## error bar ----
 arrows(
@@ -444,14 +460,16 @@ arrows(
   lwd = 0.5, angle = 90, code = 3, length = 0.02
 )
 
+
 ## axis ----
 axis(1, at = c(-10, 10), labels = F, lwd = 0.1, lwd.tick = 0.5)
 axis(2, at = seq(-10, 35, 5), lwd = 0.1, lwd.tick = 0.5, mgp = c(3, 0.2, 0))
 axis(3, at = c(-10, 10), labels = F, lwd = 0.1, lwd.tick = 0)
 axis(4, at = seq(-10, 35, 5), labels = F, lwd = 0.1, lwd.tick = 0)
 
+
 ## text ----
-mtext(1, text = 'Variables', line = label.line + 0.25, cex = cex.font - 0.3, las = 0)
-mtext(2, text = 'Relative contribution (%)', line = label.line + 0.4, cex = cex.font - 0.3, las = 0)
+mtext(1, text = 'Variables', line = label.line + 0.25, cex = cex.font - 0.35, las = 0)
+mtext(2, text = 'Relative contribution (%)', line = label.line + 0.4, cex = cex.font - 0.35, las = 0)
 mtext(3, text = substitute(bold(letter), list(letter = '(h)')), line = -1.5, adj = 0.035, cex = cex.font - 0.2)
 par(opar)
